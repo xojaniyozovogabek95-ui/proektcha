11f789aaf19954c5c2468dd2a7aafe4f572e4f7b
ogabek <xojaniyozovogabek95@gmail.com>
2025-10-28 18:16:27 +0500

    ikkinchi ishim

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const p = document.querySelectorAll("p")[0]
-const btn = document.querySelectorAll("button")[0]
-btn.onclick = function () {
-    btn.style.backgroundColor = "red"
-    btn.style.color = "black"
-    btn.textContent = "rahmat!"
+// const p = document.querySelectorAll("p")
+const btn = document.querySelector("button")
+// btn.onclick = function () {
+//     btn.style.backgroundColor = "red"
+//     btn.style.color = "black"
+//     btn.textContent = "rahmat!"
+// }
+
+const colors = ["red", "blue", "green", "yellow", "purple"]
+const div = document.querySelectorAll("div")
+btn.onclick = function(){
+    div.forEach((div) => {
+        let randoms = Math.floor(Math.random() * colors.length)
+        div.style.backgroundColor = colors[randoms]
+    });
 }
