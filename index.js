26cc62e27c8dba472ddba5caba62582c70707b26
ogabek <xojaniyozovogabek95@gmail.com>
2025-10-28 19:14:28 +0500

    uchinchi ishim

--- index.js
+++ index.js
@@ -6,11 +6,30 @@ const btn = document.querySelector("button")
 //     btn.textContent = "rahmat!"
 // }
 
-const colors = ["red", "blue", "green", "yellow", "purple"]
-const div = document.querySelectorAll("div")
-btn.onclick = function(){
-    div.forEach((div) => {
-        let randoms = Math.floor(Math.random() * colors.length)
-        div.style.backgroundColor = colors[randoms]
-    });
+// const colors = ["red", "blue", "green", "yellow", "purple"]
+// const div = document.querySelectorAll("div")
+// btn.onclick = function(){
+//     div.forEach((div) => {
+//         let randoms = Math.floor(Math.random() * colors.length)
+//         div.style.backgroundColor = colors[randoms]
+//     });
+// }
+
+
+
+const ul = document.querySelector("ul")
+const input = document.querySelector("input")
+btn.onclick = function() {
+    let value = input.value
+    if(value === "")return
+    const li = document.createElement("li")
+    li.textContent = value
+    const delbtn = document.createElement("button")
+    delbtn.textContent = "ochirish"
+delbtn.onclick = function(){
+    ul.removeChild(li)
+}
+li.appendChild(delbtn)
+ul.appendChild(li)
+input.value = ""
 }
